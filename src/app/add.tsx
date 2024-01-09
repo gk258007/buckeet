c14112a93b8fceda7f433661d68f5986280bdd15
gk258007 <gk258007@gmail.com>
2024-01-09 10:58:12 +0530

    Initial File setup

---
+++ src/app/add.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function add() {
+  return (
+    <div>Here We Add stuff to the Bucket List 🪣</div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          <nav style={{backgroundColor:'purple',textAlign:'center'}}>
+            <Link href="/" style={{margin:50}}>
+              Home
+            </Link>
+            <Link href="/add" >
+              ADD
+            </Link>
+          </nav>
+        </main>
+
+        {children}
+        
+        </body>
     </html>
   )
 }
