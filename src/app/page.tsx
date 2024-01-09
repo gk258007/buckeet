eca5f3210c45bd8e8987c3b621e1aaf1c3c31d52
gk258007 <gk258007@gmail.com>
2024-01-09 23:19:48 +0530

    Some stuffs filter and font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from 'react'
 import supabase from '../../utils/supabase'
 
 async function getData(){
-  const { data} = await supabase.from("UruSuThona").select("things_to_do");
+  const { data} = await supabase
+  .from("UruSuThona")
+  .select("things_to_do")
+  .eq("DONE","FALSE")
+  
   return {data};
 
 }
@@ -11,13 +15,15 @@ async function getData(){
     const ran_int = Math.floor(Math.random() * 10);
     // let place = data[ran_int]
     
-    // console.log(place)
+    //console.log(place)
     const { data }= await getData()
-    console.log(data[ran_int])
+    console.log(data)
   return (
   
    <div>Welcome Home today you have to do 
-    <h1>{data[ran_int].things_to_do}</h1>
+    <h1 style={{fontFamily:'Saltburn',fontSize:200,color:'yellow'}}>{data[ran_int].things_to_do}</h1>
+    <button>Did</button>
+    <button>Nope</button>
    </div>
   );
 
