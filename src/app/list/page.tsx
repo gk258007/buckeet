11db755005f7fb935b621130df5ac7912cf3a313
gk258007 <gk258007@gmail.com>
2024-01-18 19:05:20 +0530

    Add new places feature

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,17 +1,37 @@
 
 import React from 'react'
+import { createServerActionClient} from "@supabase/auth-helpers-nextjs"
+import { cookies } from 'next/headers'
+import { FormEvent } from 'react'
 import supabase from '../../../utils/supabase'
+import { revalidatePath } from 'next/cache'
 
 
 
 
 export default async function page() {
-  
+  const sendData = async (formData: FormData) =>{
+    'use server'
+    const place = formData.get('Place')
+    const Location = formData.get('Location')
+    const supabase = createServerActionClient({cookies})
+    await supabase.from('titbit').insert({ place,Location})
+    revalidatePath('/')
+  }
   
   
 
   return (
-    <div style={{fontFamily:'MADE',fontSize:30}}>Here We Add stuff to the Bucket List 🪣 
+    <div style={{fontFamily:'MADE',fontSize:30}}>
+      <h1>Here We Add stuff to the Bucket List 🪣 </h1>
+      <form style={{margin:120}} action={sendData}>
+        <label>Place</label> 
+        <input type='text' name='Place'  style={{display: 'block' ,marginBottom: 10,color:'black'}}></input>
+        <label>Location </label>
+        <input type='text' name='Location' style={{display:'block',marginBottom: 10,color:'black'}}></input>
+        <button type='submit'>Submit</button>
+      </form>
+      
     </div>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react'
 import supabase from '../../utils/supabase'
 import { UserButton } from "@clerk/nextjs";
@@ -5,26 +6,27 @@ import { UserButton } from "@clerk/nextjs";
 
 async function getData(){
   const { data} = await supabase
-  .from("UruSuThona")
-  .select("things_to_do")
-  .eq("DONE","FALSE")
+  .from("titbit")
+  .select("place")
   return {data};
 
 }
  
   export default async function page() {
-    const ran_int = Math.floor(Math.random() * 10);
-    // let place = data[ran_int]
+    const ran_int = Math.floor(Math.random() * 2);
+    //let place = data[ran_int]
     
     //console.log(place)
     const { data }= await getData()
-    console.log(data)
+    //console.log("Data from new DB",data[ran_int].place)
+    //console.log("Data ",ran_int)
   return (
    <div>
     <h1 style={{fontFamily:'MADE',fontSize:30}}>Welcome aboard today you have to go ☀️</h1>
     <div style={{marginLeft:50}}>
+     
     </div>
-    <h1 style={{fontFamily:'NeugAsia',fontSize:60,color:'yellow'}}>{data![ran_int].things_to_do}</h1>
+    <h1 style={{fontFamily:'NeugAsia',fontSize:60,color:'yellow'}}>{data![ran_int].place}</h1>  
    </div>
   );
 
