67e8630bff0c6c075ed88caa0e20ea00cfa63b72
gk258007 <gk258007@gmail.com>
2024-02-18 21:45:36 +0530

    RUH

--- src/app/component/buttonf.tsx
+++ src/app/component/buttonf.tsx
@@ -5,23 +5,23 @@ import React, { useState } from 'react'
 
 export default function Buttonf({data}:{data:any}) {
 const[resstat,setResstat] =useState(false);
-// const sendData=async()=>{
-// let resp = await updatefre()
-// if(resp.status==200)
-// {
+const sendData=async()=>{
+let resp = await updatefre()
+if(resp.status==200)
+{
 
-//   setResstat(true)
-// }else{
-//   alert("SandStorm")
-// }
-// }
+  setResstat(true)
+}else{
+  alert("SandStorm")
+}
+}
 
 const  updatefre = async()=>{
- await fetch(`./places`,{
+ let resp= await fetch(`./places`,{
         method: "put",
         body: JSON.stringify({place: data})
     });
- 
+ return resp
 }
 
   return (
@@ -30,7 +30,7 @@ const  updatefre = async()=>{
        
     
         <button onClick={updatefre} style={{fontSize:50,fontFamily:'NeugAsia'}}>LessGooooo🚀🚀</button>
-        {/* {resstat? <p style={{fontSize:50,fontFamily:'MADE'}}>Data was sent</p>:<p>DAMN!!</p>} */}
+        {resstat? <p style={{fontSize:50,fontFamily:'MADE'}}>Data was sent</p>:<p>DAMN!!</p>}
         </div>
   )
 }
