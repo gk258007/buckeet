c08eb430162045955d6e8ea10e026ee3cee8f7e3
gk258007 <gk258007@gmail.com>
2024-02-18 23:09:37 +0530

    Keys werewrong

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import supabase from '../../../utils/supabase'
 import Place from './place';
 
-
+export const dynamic = "force-dynamic";
 async function getData(){
     const { data } = await supabase.from('titbit').select('place, Location,id,Freq');
     return { data };
